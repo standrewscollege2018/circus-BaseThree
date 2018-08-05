575fd3d248131523eb9e548c9d29cae51f01a930
Benoit Wynn-Williams <benoit@wynn-williams.co.nz>
2018-08-06 10:08:53 +1200

    V1.01 - Bug Fixes
    Main Label and Selector with dynamic label is now fucntioning correctly. No known errors.

--- Concert Booking System.py
+++ Concert Booking System.py
@@ -1,7 +1,7 @@
 #Benoit Wynn-Williams
 #03 / 08 / 18 (START)
 
-### Concert Tickets###
+### Concert Tickets####
 
 #################
 #create TK window               (root can be called anything)
@@ -18,10 +18,10 @@ root.geometry('400x647')
 class Concert:
     def __init__(self, name, capacity, availability, cost):
         
-        self._name        = name       # underscore means encapsulated. so belongs (and is private) to the class.
-        self._capacity    = capacity
-        self._availablity = availability
-        self._cost        = cost
+        self._name         = name       # underscore means encapsulated. so belongs (and is private) to the class.
+        self._capacity     = capacity
+        self._availability = availability
+        self._cost         = cost
 
         concerts.append(self)
         concert_names.append(self._name)
@@ -38,11 +38,13 @@ Concert("Cultural Showcase 6PM", 220, 220, 1)
 #concert name and price overview in a dunction
 def update_concert_overview():
     concert_overview_name.set("")
+    concert_overview_capacity.set("")
+    concert_overview_availability.set("")
     concert_overview_cost.set("")
     for c in concerts:
         concert_overview_name.set(concert_overview_name.get() + c._name + "\n")
         concert_overview_capacity.set(concert_overview_capacity.get() + str(c._capacity) + "\n")
-        concert_overview_availability.set(concert_overview_availability.get() + "$" + str(c._availability) + "\n")
+        concert_overview_availability.set(concert_overview_availability.get() + str(c._availability) + "\n")
         concert_overview_cost.set(concert_overview_cost.get() + "$" + str(c._cost) + "\n")
 
 ###################
@@ -52,6 +54,8 @@ concert_name_lbl.grid(row=0, column = 0, columnspan = 4)
         
 #set up the undynamic label
 concert_overview_name = StringVar()
+concert_overview_capacity = StringVar()
+concert_overview_availability = StringVar()
 concert_overview_cost = StringVar()
 
 concert_name_lbl = Label(root, textvariable=concert_overview_name, justify=LEFT)
@@ -61,10 +65,10 @@ concert_capacity_lbl = Label(root, textvariable=concert_overview_capacity, justi
 concert_capacity_lbl.grid(row=1, column = 1)
 
 concert_availability_lbl = Label(root, textvariable=concert_overview_availability, justify=RIGHT)
-concert_availability_lbl.grid(row=1, column = 1)
+concert_availability_lbl.grid(row=1, column = 2)
 
 concert_cost_lbl = Label(root, textvariable=concert_overview_cost, justify=RIGHT)
-concert_cost_lbl.grid(row=1, column = 1)
+concert_cost_lbl.grid(row=1, column = 3)
 
 #####################
 #set up the drop down menu
@@ -86,16 +90,16 @@ selected_concert = StringVar()
 selected_concert.set(concert_names[0])
 
 concert_menu = OptionMenu(root, selected_concert, *concert_names)
-concert_menu.grid(row=1)
+concert_menu.grid(row=2)
 
 #enter button
-button_enter = Button(root, text="OK-GO", width=10, font="avenir", command=select_concert).grid(row=2, column=0)
+button_enter = Button(root, text="OK-GO", width=10, font="avenir", command=select_concert).grid(row=3, column=0)
 
 #dynamic label
 
 combined_output = StringVar()
 combined_output.set("---Not Selected---")
-combined_output_lbl = Label(root, textvariable=combined_output, font="avenir 14").grid(row=3)
+combined_output_lbl = Label(root, textvariable=combined_output, font="avenir 14").grid(row=4)
 
 concert_name = StringVar()
 '''
